6e2c9ef56fb61a49b0a1e334e5e4b6842d364bd3
Ishan1012 <ishandwivedi1210@gmail.com>
2026-06-21 16:07:18 +0530

    changed back to nodemailer

---
+++ backend/src/config/NodeMailer.ts
@@ -0,0 +1,19 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: false,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+    },
+    connectionTimeout: 20000,
+    greetingTimeout: 20000,
+    socketTimeout: 20000,
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+export default transporter;
